1194f4255414aa08e22137bacb222e4fd83d7e87
MathisNickels <114082162+MathisNickels@users.noreply.github.com>
2022-10-08 12:32:22 +0200

    Update main.c

--- lab2-gpio_library/src/main.c
+++ lab2-gpio_library/src/main.c
@@ -22,12 +22,12 @@
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>     // AVR device-specific IO definitions
 #include <util/delay.h> // Functions for busy-wait delay loops
-
+#include <gpio.h>
 
 // -----
 // This part is needed to use Arduino functions but also physical pin
 // names. We are using Arduino-style just to simplify the first lab.
-#include "Arduino.h"
+// #include "Arduino.h"
 #define PB5 13          // In Arduino world, PB5 is called "13"
 #define PB0 8
 // -----
@@ -43,40 +43,48 @@ int main(void)
 {
     uint8_t led_value = 0;  // Local variable to keep LED status
 
-    // Set pin where on-board LED is connected as output
-    pinMode(LED_GREEN, OUTPUT);
-    // Set second pin as output
-    pinMode(LED_RED, OUTPUT);
-    GPIO_mode_output(&DDRB, LED_GREEN);
-    GPIO_mode_output(&DDRB, LED_RED);
+    // Set pins where LEDs are connected as output
+
+    // Version Arduino Style
+    // pinMode(LED_GREEN, OUTPUT);
+    // pinMode(LED_RED, OUTPUT);
+    // Version Registers
+    // DDRB = DDRB | (1<<LED_GREEN);
+    // DDRB = DDRB | (1<<LED_RED);
+    //Version Library functions 
+   GPIO_mode_output(&DDRB,LED_GREEN);
+   GPIO_mode_output(&DDRB,LED_RED);
+
     // Infinite loop
     while (1)
     {
-        _delay_ms(SHORT_DELAY);
-        if(led_value==0){
-          led_value=1;
-        // Turn ON/OFF on-board LED ...
-        //digitalWrite(LED_GREEN, led_value);
-        // ... and external LED as well
-        //digitalWrite(LED_RED, led_value);
-        //PORTB=PORTB | (1<<LED_GREEN);
-        //PORTB=PORTB | (1<<LED_RED);
-        GPIO_write_high(&PORTB, LED_GREEN);
-        GPIO_write_high(&PORTB, LED_RED);
         // Pause several milliseconds
+        // _delay_ms(SHORT_DELAY);
 
         // Change LED value
+        if (led_value == 0) {
+            led_value = 1;
+            //Version Arduino style
+            // Set pin(s) to HIGH
+            // digitalWrite(LED_GREEN, HIGH);
+            // digitalWrite(LED_RED, HIGH);
+            // Version Registers
+            // PORTB = PORTB | (1<<LED_GREEN);
+            // PORTB = PORTB | (1<<LED_RED);
+            GPIO_write_high(&DDRB,LED_GREEN );
+            GPIO_write_high(&DDRB,LED_RED );
         }
-        else{
-          led_value = 0;
-          PORTB=PORTB & ~(1<<LED_RED);
-          GPIO_write_low(&PORTB, LED_GREEN);
-          GPIO_write_low(&PORTB, LED_RED);
+        else {
+            led_value = 0;
+            // Clear pin(s) to LOW
+            // digitalWrite(LED_GREEN, LOW);
+            // digitalWrite(LED_RED, LOW);
+            // Version Registers
+            // PORTB = PORTB &~ (1<<LED_GREEN);
+            // PORTB = PORTB &~ (1<<LED_RED);
+            GPIO_write_low(&DDRB,LED_GREEN );
+            GPIO_write_low(&DDRB,LED_RED );
         }
-        if (led_value == LOW)
-            led_value = HIGH;
-        else
-            led_value = LOW;
     }
 
     // Will never reach this
